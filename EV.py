2521007c8be5dac4725e611bb21c30dcac69b4e4
Michael Dickens <mdickens93@gmail.com>
2016-03-09 13:28:39 -0800

    Should really use lognormal since its tail looks similar to Pareto

--- EV.py
+++ EV.py
@@ -12,13 +12,16 @@ Probability distribution of intervention cost-effectiveness.
 
 import numpy as np
 import scipy.integrate as integrate
+import scipy.special as special
 import scipy.stats as stats
+
+robust_sigma = 1.0 # 95% chance true value is within factor of  2e (~5)
+mid_sigma = 2.0    # 95% chance true value is within factor of  6e (~16)
+weak_sigma = 6.0   # 95% chance true value is within factor of 12e (~32)
 
 x0 = 100.0
 a = 1.0/3
-sigma = 1.5
-
-# TODO: some (all?) of these PDFs should be CDFs. We can't get posterior probability that x = k but we can get probability that x < k.
+sigma = mid_sigma
 
 def pareto_pdf(x):
     global x0, a
@@ -36,10 +39,60 @@ def lognorm_cdf(x):
     global sigma
     return stats.lognorm.cdf(x, sigma)
 
-def prior_measurement(m):
+"""
+Prior probability that `Measurement = m`, possibly with an upper
+bound for utility `u`.
+"""
+def measurement_prior(m, u_max=np.inf):
+    # TODO: Python has a lot of trouble with this integral, it
+    # converges too slowly
     return integrate.quad(lambda u: lognorm_pdf(m / u) * pareto_pdf(u),
-                          0, np.inf)
+                          0, u_max, epsabs=0, limit=100)[0]
 
-print prior_measurement(100)
-print prior_measurement(1000)
-print prior_measurement(10000)
+def log_measurement_prior(m, u_max=np.inf):
+    return integrate.quad(lambda u: lognorm_pdf(m - u) * pareto_pdf(np.exp(u)))
+
+"""
+Returns a probability function `P(U|M = m) that, when called,
+gives P(U = u|M = m)`.
+"""
+def posterior(m):
+    const = measurement_prior(m)
+    def f(u):
+        prior = pareto_pdf(u)
+        update = lognorm_pdf(m / u)
+        return prior * update / const
+    return f
+
+"""
+Returns a probability function that, when called, gives
+`P(U < u|M = m)`.
+"""
+def posterior_cumulative_1(m):
+    pdf = posterior(m)
+    def f(u):
+        return integrate.quad(pdf, 0, u)[0]
+    return f
+
+"""
+Returns a probability function that, when called, gives
+`P(U < u|M = m)`.
+"""
+def posterior_cumulative_2(m):
+    const = measurement_prior(m, u_max=1e10)
+    def f(u):
+        prior = pareto_cdf(u)
+        update = measurement_prior(m, u_max=u)
+        return prior * update / const
+    return f
+
+m = 10000
+cdf = posterior_cumulative_2(m)
+def test(u):
+    global pdf, cdf
+    print "%.3f %.3f" % (pareto_cdf(u), cdf(u))
+
+test(1000)
+test(10000)
+test(100000)
+test(1000000)
